7637e31f58fcd4fd1ebcd7d5372fd17f22ea1421
Mohammed Faisal KV <30584761+faizimhmd98@users.noreply.github.com>
2022-09-28 21:34:38 +0530

    Add files via upload

---
+++ build/precache-manifest.6dc77c6c9f29e077d10e406eaf7962ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eafa52e963155f93b83",
+    "url": "/static/css/main.108454df.chunk.css"
+  },
+  {
+    "revision": "2eafa52e963155f93b83",
+    "url": "/static/js/main.2eafa52e.chunk.js"
+  },
+  {
+    "revision": "4058c2bc6402fdf67b19",
+    "url": "/static/js/1.4058c2bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1213eba79d6518d9330e1ece79b7372a",
+    "url": "/index.html"
+  }
+];
